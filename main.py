3b8aa17a8baeb02888d211e730f5d2770caaa311
Rey Arcenas <r.arcenas@ufl.edu>
2024-10-24 17:21:07 -0400

    Added context retriever

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import getpass
 import os
 
 from langchain import hub
+from langchain_community.vectorstores import FAISS
 from langchain_core.messages import HumanMessage
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import OpenAIEmbeddings
 from langchain_chroma import Chroma
 from langchain_community.document_loaders import DirectoryLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter, CharacterTextSplitter
 
 os.environ["OPENAI_API_KEY"] = getpass.getpass()
 
@@ -18,11 +19,17 @@ llm = ChatOpenAI(model="gpt-4o-mini")
 
 loader = DirectoryLoader(path="docs")
 
-docs = loader.load()
+documents = loader.load()
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-splits = text_splitter.split_documents(docs)
-vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+texts = text_splitter.split_documents(documents)
+embeddings = OpenAIEmbeddings()
+db = FAISS.from_documents(texts, embeddings)
 
-print(llm.invoke([HumanMessage(content="Hi! I'm Rey. Tell me about the Florida Alligator")]))
+retriever = db.as_retriever()
+docs = retriever.invoke("What did they say about the florida mens basketball team?")
+
+print(docs)
+
+# print(llm.invoke([HumanMessage(content="Hi! I'm Rey. Write an article from the Florida Alligator newspaper about a recent alumni.")]))
 
